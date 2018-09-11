d2e58e38e348be244b76e31dc16ffd133dc3ef3e
Sidney Alcantara <theisidney@gmail.com>
2018-09-11 16:59:54 +1000

    Week 7 lab - botmove

---
+++ botmove_digitalwrite/botmove_digitalwrite.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // Pin 13 = 5 V
+  delay(20);             // for 0.2 seconds
+  digitalWrite(13, LOW);  // Pin 13 = 0 V
+  delay(10);            // for 1.8 seconds
+}
---
+++ botmove_servolib/botmove_servolib.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>                      // Include servo library
+
+Servo servoRight;                       // Declare left servo
+Servo servoLeft;
+
+int stopValue = 1510;
+
+void setup() {
+  servoRight.attach(13); 
+  servoLeft.attach(12);  
+}
+
+void turnLeft(bool direction, int del) {
+  int multi;
+  if (direction) {
+    multi = 1;
+  } else {
+    multi = -1;
+  }
+  servoRight.writeMicroseconds(stopValue - 200 * multi);
+  servoLeft.writeMicroseconds(stopValue - 200 * multi);
+  delay(del);
+}
+
+void forward(bool direction, int del) {
+  int multi;
+  if (direction) {
+    multi = 1;
+  } else {
+    multi = -1;
+  }
+  servoRight.writeMicroseconds(stopValue - 200 * multi);
+  servoLeft.writeMicroseconds(stopValue + 200 * multi);
+  delay(del);
+}
+
+int turnTime = 1000;
+void loop() {
+    turnLeft(true, turnTime);
+    turnLeft(false, turnTime);
+}
+
